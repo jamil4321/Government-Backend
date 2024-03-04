b664757d20742ded67f58328e26a382eb5adf68c
Jamil Ahmed <jamil.ahmed.ja28@gmail.com>
2024-03-04 15:05:28 +0500

    update

--- userDetails.js
+++ userDetails.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const UserDetailsScehma  = new mongoose.Schema(
   {
+    name:String,
     email: { type: String, unique: true },
     password: String,
     location:String,
